2999dd0c5a9d157a3c7ca239bee8c7c98b020a49
Zrossiz <soon.pluxury@mail.ru>
2024-04-29 21:45:15 +0300

    refactor: renamed app

--- StormTalk/urls.py
+++ StormTalk/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for social_network project.
+URL configuration for StormTalk project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
