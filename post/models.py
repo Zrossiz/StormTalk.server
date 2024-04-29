9f02a91ec05c5909f0e668a9e5d27cccc4ecd4c5
Zrossiz <soon.pluxury@mail.ru>
2024-04-29 18:14:05 +0300

    feat: added post model

--- post/models.py
+++ post/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from user.models import User
 
 # Create your models here.
+class Post(models.Model):
+    image = models.CharField(max_length=500)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'post'
