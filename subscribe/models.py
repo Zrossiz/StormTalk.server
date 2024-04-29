fcb8f24634903eebc7ebc860e2d7a1e444b339ea
Zrossiz <soon.pluxury@mail.ru>
2024-04-29 21:25:05 +0300

    feat: added subscribe model

--- subscribe/models.py
+++ subscribe/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from user.models import User
 
 # Create your models here.
+class Subscribe(models.Model):
+    listener = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listener_id")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author_id")
+
+    class Meta: 
+        db_table = 'subscribe'
