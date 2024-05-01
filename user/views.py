42717c3582be0ef7afe9fd2d167c932ae56d4c5c
Zrossiz <soon.pluxury@mail.ru>
2024-05-01 16:36:34 +0300

    feat: added error handler

--- user/views.py
+++ user/views.py
@@ -10,39 +10,85 @@ from .models import User
 class RegistrationUserAPIView(APIView):
 
     def post(self, request):
+        try:
+            data = request.data
+            if 'password' not in data:
+                return Response({
+                    'success': False,
+                    'message': 'password not found'
+                }, status=status.HTTP_400_BAD_REQUEST)
+            
+            if 'username' not in data:
+                return Response({
+                    'success': False,
+                    'message': 'username not found'
+                }, status=status.HTTP_400_BAD_REQUEST)
+            
+            if 'email' not in data:
+                return Response({
+                    'success': False,
+                    'message': 'email not found'
+                }, status=status.HTTP_400_BAD_REQUEST)
+            
 
-        salt = bcrypt.gensalt()
-        request.data['password'] = bcrypt.hashpw(request.data['password'].encode('utf-8'), salt).decode('utf-8')
+            salt = bcrypt.gensalt()
+            data['password'] = bcrypt.hashpw(data['password'].encode('utf-8'), salt).decode('utf-8')
 
-        serializer = UserSerializer(data=request.data)
+            serializer = UserSerializer(data=data)
 
-        if serializer.is_valid():
+            if serializer.is_valid():
 
-            user = serializer.save()
+                user = serializer.save()
 
-            token = AuthenticationView().get_token(user)
+                token = AuthenticationView().get_token(user)
 
+                return Response({
+                    'token': str(token),
+                    'user': serializer.data,
+                }, status=status.HTTP_201_CREATED)
+        except Exception as e:
             return Response({
-                'token': str(token),
-                'user': serializer.data,
-            }, status=status.HTTP_201_CREATED)
+                'success': False,
+                'message': str(e)
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
         
 class LoginUserAPIView(APIView):
 
     def post(self, request):
+        try:
+            data = request.data
 
+            if 'password' not in data:
+                return Response({
+                    'success': False,
+                    'message': 'password not found'
+                }, status=status.HTTP_400_BAD_REQUEST)
+            
+        
+            if 'username' not in data:
+                return Response({
+                    'success': False,
+                    'message': 'username not found'
+                }, status=status.HTTP_400_BAD_REQUEST)
+            
+            user = User.objects.get(username=data['username'])
 
-        data = request.data
-        user = User.objects.get(username=data['username'])
-        serializer = UserSerializer(user).data
+            serializer = UserSerializer(user).data
 
-        if bcrypt.checkpw(data['password'].encode('utf-8'), serializer['password'].encode('utf-8')):
-            token = AuthenticationView().get_token(user)
+            if bcrypt.checkpw(data['password'].encode('utf-8'), serializer['password'].encode('utf-8')):
+                token = AuthenticationView().get_token(user)
 
+                return Response({
+                    'success': True,
+                    'token': token,
+                    'user': serializer
+                }, status=status.HTTP_200_OK)
+        except Exception as e:
             return Response({
-                'token': token,
-                'user': serializer
-            }, status=status.HTTP_200_OK)
+                'success': False,
+                'message': str(e)
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
        
 
 
