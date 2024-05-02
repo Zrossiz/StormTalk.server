e7f8d3b1f2708159ef54eb2d0d43fbf3c4fa734c
Zrossiz <soon.pluxury@mail.ru>
2024-05-02 21:34:45 +0300

    feat: added caht model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from user.models import User
+import uuid
 
-# Create your models here.
+class Chat(models.Model):
+    initiator = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="initiator_chat"
+    )
+    acceptor = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="acceptor_name"
+    )
+    short_id = models.CharField(max_length=255, default=uuid.uuid4, unique=True)
+
+    class Meta:
+        db_table = 'chat'
--- message/models.py
+++ message/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from user.models import User
+from chat.models import Chat
 
 
 class Message(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name="messages")
     text = models.CharField(max_length=2500)
     read = models.BooleanField(False)
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_message")
