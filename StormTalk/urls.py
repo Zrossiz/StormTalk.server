654ae4278363aefb97e9af9d57a15f6eaa73a49b
Zrossiz <soon.pluxury@mail.ru>
2024-05-05 09:54:56 +0300

    chore: updated docs

--- StormTalk/urls.py
+++ StormTalk/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for StormTalk project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
@@ -23,12 +7,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Your API",
+      title="StormTalk",
       default_version='v1',
-      description="Your API description",
-      terms_of_service="https://www.example.com/policies/terms/",
-      contact=openapi.Contact(email="contact@example.com"),
-      license=openapi.License(name="BSD License"),
+      description="API docs for StormTalk server",
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
@@ -41,6 +22,6 @@ urlpatterns = [
     path('api/post/', include('post.urls')),
     path('api/subscribe/', include('subscribe.urls')),
     path('api/news/', include('news.urls')),
-    path('api/chat', include('chat.urls')),
+    path('api/chat/', include('chat.urls')),
     path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
