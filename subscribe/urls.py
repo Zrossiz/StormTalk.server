968f9f67ac9f9d52c789504d115dfe2df5414a27
Zrossiz <soon.pluxury@mail.ru>
2024-05-05 11:07:14 +0300

    feat: added delete subscribe route

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateSubcsribeAPIView
+from .views import CreateSubcsribeAPIView, UpdateSubscribeAPIView
 
 urlpatterns = [
-    path('', CreateSubcsribeAPIView.as_view())
+    path('', CreateSubcsribeAPIView.as_view()),
+    path('<int:post_id>', UpdateSubscribeAPIView.as_view())
 ]
--- subscribe/views.py
+++ subscribe/views.py
@@ -5,6 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from user.models import User
+from .models import Subscribe
 
 
 class CreateSubcsribeAPIView(APIView):
@@ -42,3 +43,33 @@ class CreateSubcsribeAPIView(APIView):
                 'success': False,
                 'message': str(e)
             }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class UpdateSubscribeAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, post_id):
+        try:
+            data = request.data
+            user_id = int(data['user'].id)
+            
+            subscribe = Subscribe.objects.get(pk=post_id)
+            serializer = SubscribeSerializer(subscribe).data
+            if serializer['listener'] == user_id:
+                subscribe.delete()
+                return Response({
+                    'success': True,
+                    'data': serializer
+                }, status=status.HTTP_200_OK)
+            else:
+                return Response({
+                    'success': False,
+                    'message': 'Forbidden'
+                }, status=status.HTTP_403_FORBIDDEN)
+        except Exception as e:
+            return Response({
+                'success': False,
+                'message': str(e)
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
